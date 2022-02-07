9013d50035c7726b984d4c4715dbe11d2de54f63
sadafghadiyali <sadafghadiyali@gmail.com>
2022-02-07 10:41:15 +0530

    Java Collections

---
+++ Section 12/HashMap/Location.java
@@ -0,0 +1,32 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class Location {
+
+    private final Map<String, Integer> exits;
+    private final int locationID;
+    private final String description;
+
+    public Location(int locationID, String description) {
+        this.locationID = locationID;
+        this.description = description;
+        this.exits=new HashMap<>();
+        this.exits.put("Q",0);
+    }
+
+    public Map<String, Integer> getExits() {
+        return new HashMap<String,Integer>(exits);
+    }
+
+    public int getLocationID() {
+        return locationID;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void addExit(String direction, int locationID){
+        this.exits.put(direction,locationID);
+    }
+}
